c9eb525061194384931a21c71fc096afdb593b77
kanaenumatalee@gmail.com <83003828+infprogrammer@users.noreply.github.com>
2022-11-02 15:18:42 -0700

    fixed typo on landmark table, changed to alter table itinerary

--- java/database/landmarks.sql
+++ java/database/landmarks.sql
@@ -1,17 +1,13 @@
-CREATE TABLE itinerary (
-	itinerary_id serial NOT NULL PRIMARY KEY UNIQUE,
-    landmark_id serial NOT NULL,
-	itinerary_name varchar(50) NOT NULL,
-	starting_point varchar(50) NOT NULL,
-	itinerary_date date NOT NULL,
-	user_id int NOT NULL,
-	constraint fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
-    constraint fk_landmark_id FOREIGN KEY (landmark_id) REFERENCES landmark(landmark_id)
-)
-
 CREATE TABLE landmark (
 	landmark_id serial NOT NULL PRIMARY KEY UNIQUE,
-	lanmark_name varchar(50) NOT NULL,
+	landmark_name varchar(50) NOT NULL,
 	category varchar(40) NOT NULL,
 	description text NOT NULL
+)
+
+ALTER TABLE itinerary (
+    ADD landmark_id serial NOT NULL,
+    ADD CONSTRAINT fk_landmark_id
+        FOREIGN KEY (landmark_id)
+        REFERENCES landmark(landmark_id)
 )
