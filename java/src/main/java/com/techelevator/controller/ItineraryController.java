333507a595cd721d52c9f0b1c6e90334d39d19b1
Kanae Lee <kanaenumatalee@gmail.com>
2022-11-04 15:58:32 +0000

    Update ItineraryController.java

--- java/src/main/java/com/techelevator/controller/ItineraryController.java
+++ java/src/main/java/com/techelevator/controller/ItineraryController.java
@@ -2,6 +2,8 @@ package com.techelevator.controller;
 
 import com.techelevator.model.Itinerary;
 import com.techelevator.dao.ItineraryDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.dao.LandmarkDao;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,11 @@ import javax.validation.Valid;
 public class ItineraryController {
     @Autowired
     ItineraryDao itineraryDao;
+    /*@Autowired
+    UserDao userDao;
+    @Autowired
+    LandmarkDao landmarkDao;
+    */
 
     public ItineraryController(ItineraryDao itineraryDao) {
         this.itineraryDao = itineraryDao;
@@ -30,5 +37,9 @@ public class ItineraryController {
         String startingPoint = itinerary.getStartingPoint();
         Date itineraryDate = itinerary.getItineraryDate();
         itineraryDao.createItinerary(itineraryId, itineraryName, startingPoint, itineraryDate);
+        //long userId = user.getId();
+        //int landmarkId = landmark.getId();
+        //itineraryDao.createItinerary(itineraryId, itineraryName, startingPoint, itineraryDate, userId, landmarkId);
+        
     }
 }
