b3c61d0374c34e152eb33d3e2f43033554900393
Julian Prodnuk <Oddweather1@gmail.com>
2022-11-22 15:52:42 -0600

    RatingsTesting

---
+++ java/src/main/java/com/techelevator/dao/JdbcLikesDao.java
@@ -0,0 +1,150 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Landmark;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class JdbcLandmarkDao implements LandmarkDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcLandmarkDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Landmark> allLandmarks() {
+        List<Landmark> landmarks = new ArrayList<>();
+        String sql = "SELECT * FROM landmark ORDER BY landmark_id ASC";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+
+        while (rowSet.next())
+        {
+            landmarks.add(mapRowToLandmark(rowSet));
+        }
+        return landmarks;
+    }
+
+    @Override
+    public Landmark getLandmarkById(int landmarkId) {
+        Landmark landmark = new Landmark();
+        String sql = "SELECT * FROM landmark WHERE landmark_id = ?;";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, landmarkId);
+
+        if (rowSet.next())
+        {
+            return mapRowToLandmark(rowSet);
+        }
+
+        return landmark;
+    }
+
+    @Override
+    public List<Landmark> getLandmarkByStateCity(String landmarkState, String landmarkCity) {
+        List<Landmark> landmarks = new ArrayList<>();
+        String sql = "SELECT * FROM landmark WHERE state ILIKE ? AND city ILIKE ?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, landmarkState, landmarkCity);
+
+        while (rowSet.next())
+        {
+            landmarks.add(mapRowToLandmark(rowSet));
+        }
+        return landmarks;
+    }
+
+    @Override
+    public List<String> getStates() {
+        List<Landmark> landmarks = new ArrayList<>();
+        List<String> states = new ArrayList<>();
+
+        String sql = "SELECT DISTINCT state from landmark";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+
+        while (rowSet.next())
+        {
+            landmarks.add(mapRowToState(rowSet));
+        }
+
+        for (Landmark lm: landmarks) {
+            states.add(lm.getState());
+        }
+
+        return states;
+    }
+
+    @Override
+    public List<String> getCities() {
+        List<Landmark> landmarks = new ArrayList<>();
+        List<String> cities = new ArrayList<>();
+
+        String sql = "SELECT DISTINCT city from landmark";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+
+        while (rowSet.next())
+        {
+            landmarks.add(mapRowToCities(rowSet));
+        }
+
+        for (Landmark lm: landmarks) {
+            cities.add(lm.getCity());
+        }
+
+        return cities;
+    }
+
+    @Override
+    public List<String> getCityInState(String state) {
+        List<Landmark> landmarks = new ArrayList<>();
+        List<String> cities = new ArrayList<>();
+
+        String sql = "SELECT DISTINCT city from landmark WHERE state ILIKE ?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, state);
+
+        while (rowSet.next())
+        {
+            landmarks.add(mapRowToCities(rowSet));
+        }
+
+        for (Landmark lm: landmarks) {
+            cities.add(lm.getCity());
+        }
+
+        return cities;
+    }
+
+
+    private Landmark mapRowToLandmark(SqlRowSet rs) {
+        Landmark landmark = new Landmark();
+        landmark.setLandmarkId(rs.getInt("landmark_id"));
+        landmark.setLandmarkName(rs.getString("landmark_name"));
+        landmark.setAddress(rs.getString("address"));
+        landmark.setCity(rs.getString("city"));
+        landmark.setState(rs.getString("state"));
+        landmark.setPostalCode(rs.getString("postal_code"));
+        landmark.setCategory(rs.getString("category"));
+        landmark.setDescription(rs.getString("description"));
+        landmark.setImgUrl(rs.getString("image_url"));
+        landmark.setMapUrl(rs.getString("map_url"));
+        landmark.setLikes(rs.getString("likes"));
+        landmark.setDislikes(rs.getString("dislikes"));
+        return landmark;
+    }
+
+    private Landmark mapRowToState(SqlRowSet rs) {
+        Landmark landmark = new Landmark();
+        landmark.setState(rs.getString("state"));
+        return landmark;
+    }
+
+    private Landmark mapRowToCities(SqlRowSet rs) {
+        Landmark landmark = new Landmark();
+        landmark.setCity(rs.getString("city"));
+        return landmark;
+    }
+
+}
---
+++ java/src/main/java/com/techelevator/model/Likes.java
@@ -0,0 +1,131 @@
+package com.techelevator.model;
+
+public class Landmark {
+
+    private int landmarkId;
+    private String landmarkName;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String category;
+    private String description;
+    private String imgUrl;
+    private String likes;
+    private String dislikes;
+
+    private String mapUrl;
+    private int getLandmarkId() {
+        return landmarkId;
+    }
+
+    public void setLandmarkId(int landmarkId) {
+        this.landmarkId = landmarkId;
+    }
+
+    public String getLandmarkName() {
+        return landmarkName;
+    }
+
+    public void setLandmarkName(String landmarkName) {
+        this.landmarkName = landmarkName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getMapUrl() {
+        return mapUrl;
+    }
+
+    public void setMapUrl(String mapUrl) {
+        this.mapUrl = mapUrl;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public String getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(String dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    @Override
+    public String toString() {
+        return "Landmark{" +
+                "landmarkId=" + landmarkId +
+                ", landmarkName='" + landmarkName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", category='" + category + '\'' +
+                ", description='" + description + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", mapUrl='" + mapUrl + '\'' +
+                ", likes='" + likes + '\'' +
+                ", dislikes='" + dislikes + '\'' +
+                '}';
+    }
+}
