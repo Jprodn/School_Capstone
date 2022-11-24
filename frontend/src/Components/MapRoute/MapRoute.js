61568354e72934b8ad43abfd07a27fffec533d26
Juan <juanhernandez0333@gmail.com>
2022-11-23 20:40:32 -0600

    Added routing from coords needs changes to change coords on itinerary points

--- frontend/src/Components/MapRoute/MapRoute.js
+++ frontend/src/Components/MapRoute/MapRoute.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import L from "leaflet";
+import MapRouter from "./MapRouter";
 
 function MapRoute(props) {
   return (
@@ -13,12 +16,21 @@ function MapRoute(props) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[props.latitude, props.longitude]}>
+        {/* <Marker position={[props.latitude, props.longitude]}>
           <Popup>Your location</Popup>
-        </Marker>
+        </Marker> */}
+        <MapRouter />
       </MapContainer>
     </div>
   );
 }
+
+let defaulIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
+L.Marker.prototype.options.icon = defaulIcon;
 
 export default MapRoute;
