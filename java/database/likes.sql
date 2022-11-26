ab9e9fbe11656c73c14390b42eb2706990fd9555
Juan <juanhernandez0333@gmail.com>
2022-11-26 00:30:02 -0600

    Changes to likes db

--- java/database/likes.sql
+++ java/database/likes.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS likes (
     constraint fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id),
 	constraint fk_landmark_id FOREIGN KEY(landmark_id) REFERENCES landmark(landmark_id)
 )
+
+INSERT INTO likes(like_id, user_id, landmark_id, like_type)
+VALUES(DEFAULT, 3, 1, 2)
+
+SELECT * FROM Likes
