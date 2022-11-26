e1b785c0839ccf7fe3a65cb05452f1983222cc24
Abdo <mohamabdo@yahoo.com>
2022-11-26 05:13:39 -0800

    added some styling for navbar

--- frontend/src/Components/Routers/NavLinkBar.js
+++ frontend/src/Components/Routers/NavLinkBar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 
 export default function NavLinkBar(props) {
   const handleLogout = () => {
@@ -12,24 +13,59 @@ export default function NavLinkBar(props) {
   console.log("%c-----NavLinkBar-----", "color: yellow; background-color: black");
   console.log("%cToken", "color: yellow; background-color: black", token);
 
+  
+
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+
   return (
     <>
-      <Link className="main-list" to="/home">
+
+<nav className="nav">
+<Link className="main-list"  to="/home">
         Home{" "}
       </Link>
-      <Link className="main-list" to="/itinerary">
+      <ul className={active}>
+      
+        <li className="nav__item">
+        <Link className="main-list"  to="/itinerary">
         Itinerary{" "}
       </Link>
-      <Link className="main-list" to="/itinerary/create">
+        </li>
+        <li className="nav__item">
+        <Link className="main-list"  to="/itinerary/create">
         Create{" "}
       </Link>
-      <Link className="main-list" to="/landmark">
+        </li>
+        <li className="nav__item">
+        <Link className="main-list" to="/landmark">
         Search{" "}
       </Link>
+        </li>
 
-      <Link className="main-list" to="/login" onClick={handleLogout}>
+        <li className="nav__item">
+        <Link className="main-list" to="/login" onClick={handleLogout}>
         {token !== undefined ? "logout" : "login"}
       </Link>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+
     </>
   );
 }
