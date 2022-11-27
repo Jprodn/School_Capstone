5a60888dd591acfbbe0ca8aab6c95027c75ee155
kanaenumatalee@gmail.com <83003828+infprogrammer@users.noreply.github.com>
2022-11-27 12:35:00 -0800

    added rating.sql insert data for table

--- java/database/rating.sql
+++ java/database/rating.sql
@@ -5,3 +5,19 @@ CREATE TABLE IF NOT EXISTS rating (
 	constraint PK_rating PRIMARY KEY(landmark_id),
 	constraint fk_landmark_id FOREIGN KEY(landmark_id) REFERENCES landmark(landmark_id)
 )
+
+insert into rating (landmark_id, likes, dislikes) values ('1','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('2','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('3','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('4','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('5','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('6','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('7','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('8','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('9','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('10','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('11','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('12','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('13','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('14','0','0');
+insert into rating (landmark_id, likes, dislikes) values ('15','0','0');
